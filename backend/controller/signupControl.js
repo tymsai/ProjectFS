a7ce56403bb8cb7c7bea8fc88690f319d21e5d9d
tymsai <tymsai6076@gmail.com>
2024-08-23 18:56:30 +0530

    mongoDB not initiated, searching for content on token

--- backend/controller/signupControl.js
+++ backend/controller/signupControl.js
@@ -1,10 +1,23 @@
+const bcrypt=require("bcrypt")
+const jwt = require("jsonwebtoken")
+const UserModel=require("../models/UserModel")
 const signupRequest = async (req, res)=>{
     try{
-        //const body=req.body
-        console.log(req.body)
-        return res.json({"verification": "true"});
+        const {username, email, password}= req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const usernameFind=await UserModel.findOne({username: username})
+        console.log(usernameFind)
+        const emailFind=await UserModel.findOne({email: email})
+        console.log(emailFind)
+        if(!emailFind && !usernameFind){
+            throw new Error("Email & Username either of them Exists")
+        }
+        //const userAdded=await UserModel.create({username, email, hashedPassword})
+        const token = jwt.sign(buildToken(userAdded),process.env.JWT_TOKEN_SECRET)
+        
+        return res.json({"verification": token});
     }catch (error){
-        return res.error
+        return res.status(400).json({ error: error.message });
     }
 }
 module.exports = {signupRequest};
