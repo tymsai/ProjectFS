f04019a440b49843b8f6172676a91e11773cc680
tymsai <tymsai6076@gmail.com>
2024-08-24 20:43:55 +0530

    JWT

--- backend/controller/signupControl.js
+++ backend/controller/signupControl.js
@@ -4,16 +4,26 @@ const UserModel=require("../models/UserModel")
 const signupRequest = async (req, res)=>{
     try{
         const {username, email, password}= req.body
+        console.log(username)
+        console.log(email)
+        console.log(password)
+        const tok=req.headers["token"]
+        console.log(tok)
+        const vari=jwt.verify(tok,process.env.JWT_TOKEN_SECRET)
+        console.log("vari -", vari)
         const hashedPassword = await bcrypt.hash(password, 10)
         const usernameFind=await UserModel.findOne({username: username})
         console.log(usernameFind)
         const emailFind=await UserModel.findOne({email: email})
         console.log(emailFind)
+
+        const token = jwt.sign({username, hashedPassword},process.env.JWT_TOKEN_SECRET)
+        return res.json({"token": token});
         if(!emailFind && !usernameFind){
             throw new Error("Email & Username either of them Exists")
         }
+        
         //const userAdded=await UserModel.create({username, email, hashedPassword})
-        const token = jwt.sign(buildToken(userAdded),process.env.JWT_TOKEN_SECRET)
         
         return res.json({"verification": token});
     }catch (error){
